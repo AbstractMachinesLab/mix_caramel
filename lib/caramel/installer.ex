9adcbb433285f2f117a418f4102720e974a0fa98
Drew Olson <drew@drewolson.org>
2021-05-04 14:47:00 -0500

    feat: verify tar and wget are available for the installer (#5)

--- lib/caramel/installer.ex
+++ lib/caramel/installer.ex
@@ -23,10 +23,19 @@ defmodule Caramel.Installer do
     Mix.Shell.cmd("mv caramel _build/", [], fn _ -> :ok end)
   end
 
+  defp verify_prereqs(prereqs) do
+    Enum.each(prereqs, fn prereq ->
+      if Mix.Shell.Quiet.cmd("which " <> prereq) != 0 do
+        Mix.raise(prereq <> " is requred to install caramel.")
+      end
+    end)
+  end
+
   def install(version) do
     host_triplet = get_host_triplet()
     tarball_name = tarball_name(version, host_triplet)
     release_url = release_url(version, tarball_name)
+    verify_prereqs(["wget", "tar"])
     download_caramel(version, tarball_name, release_url)
   end
 end
